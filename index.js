11bdf0ae2905b3a8ace00ed3f2c086ce54e7e233
neolao <contact@neolao.com>
2019-10-02 20:09:40 +0000

    no-bitwise off

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ module.exports = {
         "no-restricted-syntax": "off",
         "guard-for-in": "off",
         "no-await-in-loop": "off",
-        "no-underscore-dangle": "off"
+        "no-underscore-dangle": "off",
+        "no-bitwise": "off"
       }
     }
   }
